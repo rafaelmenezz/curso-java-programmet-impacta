e55518108f7c3c334f62bd65c4847f55d83b1721
fael_job <rafael.menezes@alunos.usj.edu.br>
2022-07-15 11:25:51 -0300

    Start curso Java Programmer

---
+++ Projeto01_Conceitos/src/curso/java/veolia/SegundoPrograma.java
@@ -0,0 +1,15 @@
+package curso.java.veolia;
+
+import javax.swing.JOptionPane;
+
+/*
+ * Criação de pacotes
+ * 
+ * 
+ */
+
+public class SegundoPrograma {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "Segundo Programa");
+	}
+}
